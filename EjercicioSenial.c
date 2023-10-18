a428eabf580e42dfbc98444bc675b70fd04f9b84
Thomas <117021963+thomaswillix@users.noreply.github.com>
2023-10-18 13:44:53 +0200

    Add files via upload

---
+++ EjercicioSenial.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "cosillas.h"
+#include <stdbool.h>
+
+pid_t pid_hijo;
+
+
+void manejador (int segnal)
+{
+    printf("Por esta vez te has salvado, no te fíes de los extraños\n");
+    kill(pid_hijo,SIGKILL); 
+}
+bool comprobacion ()
+{
+    int anio;
+    scanf("%d",&anio);
+	while(anio != 1989){
+        printf("error\n");
+        scanf("%d",&anio);        
+    }
+    printf("correcto\n");
+    return true;
+}
+
+int main(){
+	int num;
+    
+
+	pid_hijo=fork();
+    switch(pid_hijo){
+		case -1:
+			printf("error al crear el proceso\n");
+			exit(-1);
+		case 0:
+        printf("¿En qué año ganó la vuelta a España Pedro Delgado por primera vez?\n¡Si no aciertas en 30 segundos destruiré todos tus datos!\n");
+
+            if (comprobacion())
+            {
+                signal(SIGUSR1 ,manejador);    
+            }
+		default:
+            sleep(5);
+            destruir();
+            kill(pid_hijo,SIGKILL);
+	}
+	return(0);
+}
